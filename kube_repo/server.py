5d7021d4de8e40b4f7e01e0e66ed3c03b002fd49
Kondrahin Anton <56519069+Kondrahin@users.noreply.github.com>
2020-11-16 13:41:32 +0300

    Update server.py

--- kube_repo/server.py
+++ kube_repo/server.py
@@ -8,8 +8,8 @@ from _thread import *
 list_of_clients = []
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server.bind(('', 9090))
-server.listen(1000)
+server.bind(('', 8000))
+server.listen(10000000)
 token = "3ce965ac7f9328"
 
 
@@ -30,16 +30,24 @@ def client_thread(conn: socket, addr):
                            'message': 'What\'s your name?'})
     conn.send(bytes(question, encoding='utf8'))
     username = conn.recv(1024).decode("utf-8")
+
     if len(username) == 0 or username[0:14] != token:
         remove(curr_connection=conn)
         conn.close()
+
     greeting_message = "Welcome to the club " + username[14:]
     greeting = json.dumps({'hour': time.localtime().tm_hour + 3, 'minute': time.localtime().tm_min, 'nick': 'server',
                            'message': greeting_message})
-    conn.send(bytes(greeting, encoding='utf8'))
+    try:
+        conn.send(bytes(greeting, encoding='utf8'))
+    except Exception as e:
+        print("User not auth")
+        conn.close()
+        remove(conn)
+
     broadcast_meeting(username=username[14:], connection=conn)
-    while True:
-        try:
+    try:
+        while True:
             message = conn.recv(1024).decode("utf-8")
             auth(message=message)
             if len(message) > 0:
@@ -50,13 +58,17 @@ def client_thread(conn: socket, addr):
                 broadcast(message_to_send, conn)
 
             else:
-                # print("except")
-                # conn.close()
+                conn.close()
                 remove(conn)
+    except ConnectionResetError:
+        print("Connection lost")
+        conn.close()
+        remove(conn)
 
-        except (KeyboardInterrupt, SystemExit):
-            print("ex")
-            # conn.close()
+    except Exception as e:
+        print(str(e))
+        conn.close()
+        remove(conn)
 
 
 def broadcast_meeting(username, connection):
@@ -64,22 +76,23 @@ def broadcast_meeting(username, connection):
         {'hour': time.localtime().tm_hour + 3, 'minute': time.localtime().tm_min, 'nick': 'server',
          'message': username + ' join us'})
     for clients in list_of_clients:
-        if clients != connection:
-            clients.send(bytes(notification, encoding='utf8'))
+        # if clients != connection:
+        clients.send(bytes(notification, encoding='utf8'))
 
 
 def broadcast(message, connection):
     for clients in list_of_clients:
-        if clients != connection:
-            try:
-                clients.send(bytes(message, encoding='utf8'))
-            except:
-                print(connection + " remove")
-                clients.close()
-                remove(clients)
+        # if clients != connection:
+        try:
+            clients.send(bytes(message, encoding='utf8'))
+        except:
+            print(connection + " remove")
+            clients.close()
+            remove(clients)
 
 
 try:
+    print("Server launch")
     client, addr = server.accept()
     list_of_clients.append(client)
     print(addr[0] + " connected")
@@ -89,7 +102,8 @@ try:
         list_of_clients.append(client)
         print(addr[0] + " connected")
         start_new_thread(client_thread, (client, addr))
-except(KeyboardInterrupt, SystemExit):
+except KeyboardInterrupt as e:
+    print(str(e))
     for connection in list_of_clients:
         print("Keyboard interrupt")
         connection.close()
